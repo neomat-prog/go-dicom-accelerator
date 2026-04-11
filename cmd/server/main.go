5d0c7da3a948929b0d7e0f8dc2fdd1d93e90aea4
neomat-prog <thegreyliongaming@gmail.com>
2026-04-11 21:08:26 +0200

    feat: add DICOM handler to serve .dcm files

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,9 +10,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+func dicomHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("./test.dcm")
+	if err != nil {
+		http.Error(w, "file not found", http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		http.Error(w, "could not get file info", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/dicom")
+	w.Header().Set("Content-Disposition", `inline; filename="test.dcm"`)
+
+	http.ServeContent(w, r, "test.dcm", info.ModTime(), file)
+}
+
 func main() {
+	http.HandleFunc("/dicom", dicomHandler)
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +43,7 @@ func main() {
 	})
 
 	log.Println("server is running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
