58cd4e896afa474ace8c03816e9e14206fc3d734
neomat-prog <thegreyliongaming@gmail.com>
2026-05-02 14:48:16 +0200

    feat: implement source accelerator with window options for fetching instances

--- dicomfetch/accelerator.go
+++ dicomfetch/accelerator.go
@@ -1,16 +1,26 @@
 package dicomfetch
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"io"
+	"sync"
 	"time"
 
 	"github.com/neomat-prog/go-dicom-gateway/source"
 )
 
+/*
+	WindowBehind = how many previous instances to warm
+	WindowAhead = how many next instances to prefetch
+*/
+
 type Options struct {
 	MaxConcurrency int
 	BatchSize      int
+	WindowAhead    int
+	WindowBehind   int
 	RequestTimeout time.Duration
 }
 
@@ -19,14 +29,40 @@ type Fetcher struct {
 	Options Options
 }
 
-/*
-	WindowBehind = how many previous instances to warm
-	WindowAhead = how many next instances to prefetch
-*/
+func DefaultOptions() Options {
+	return Options{
+		MaxConcurrency: 8,
+		BatchSize:      32,
+		WindowAhead:    16,
+		WindowBehind:   4,
+		RequestTimeout: 15 * time.Second,
+	}
+}
 
-type Window struct {
-	WindowAhead  int
-	WindowBehind int
+func (o Options) Normalize() Options {
+	if o.MaxConcurrency <= 0 {
+		o.MaxConcurrency = 1
+	}
+	if o.BatchSize < 0 {
+		o.BatchSize = 0
+	}
+	if o.WindowAhead < 0 {
+		o.WindowAhead = 0
+	}
+	if o.WindowBehind < 0 {
+		o.WindowBehind = 0
+	}
+	if o.RequestTimeout < 0 {
+		o.RequestTimeout = 0
+	}
+	return o
+}
+
+func New(src source.Source, options Options) *Fetcher {
+	return &Fetcher{
+		Source:  src,
+		Options: options.Normalize(),
+	}
 }
 
 /*
@@ -69,5 +105,110 @@ func SelectWindow(refs []source.InstanceRef, center int, behind int, ahead int) 
 		end = len(refs)
 	}
 
-	return refs[start:end], nil
+	window := make([]source.InstanceRef, end-start)
+	copy(window, refs[start:end])
+
+	return window, nil
+}
+
+func (f *Fetcher) FetchInstance(ctx context.Context, ref source.InstanceRef) (source.Response, error) {
+	if f == nil {
+		return source.Response{}, errors.New("fetcher cannot be nil")
+	}
+
+	if f.Source == nil {
+		return source.Response{}, errors.New("fetcher source cannot be nil")
+	}
+
+	if f.Options.RequestTimeout > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, f.Options.RequestTimeout)
+		defer cancel()
+	}
+
+	return f.Source.Instance(ctx, ref)
+}
+
+func (f *Fetcher) FetchWindow(ctx context.Context, refs []source.InstanceRef, center int) ([]source.Response, error) {
+
+	if f == nil {
+		return nil, errors.New("dicomfetch: nil fetcher")
+	}
+
+	options := f.Options.Normalize()
+
+	window, err := SelectWindow(refs, center, options.WindowBehind, options.WindowAhead)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	responses := make([]source.Response, len(window))
+	sem := make(chan struct{}, options.MaxConcurrency)
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var errs []error
+
+	for i, ref := range window {
+		select {
+		case <-ctx.Done():
+			break
+		case sem <- struct{}{}:
+		}
+
+		wg.Add(1)
+
+		go func(i int, ref source.InstanceRef) {
+			defer wg.Done()
+			defer func() { <-sem }()
+
+			resp, err := f.FetchInstance(ctx, ref)
+			if err != nil {
+				mu.Lock()
+				errs = append(errs, fmt.Errorf("fetch instance %q: %w", ref.SOPInstanceUID, err))
+				mu.Unlock()
+
+				cancel()
+				return
+			}
+
+			responses[i] = resp
+		}(i, ref)
+	}
+
+	wg.Wait()
+
+	if len(errs) > 0 {
+		closeResponses(responses)
+		return nil, errors.Join(errs...)
+	}
+
+	if err := ctx.Err(); err != nil {
+		closeResponses(responses)
+		return nil, err
+	}
+
+	return responses, nil
+
+}
+
+func closeResponses(responses []source.Response) {
+	for _, resp := range responses {
+		if resp.Body != nil {
+			_ = resp.Body.Close()
+		}
+	}
+}
+
+func ReadAllAndClose(resp source.Response) ([]byte, error) {
+	if resp.Body == nil {
+		return nil, errors.New("dicomfetch: response body is nil")
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
 }
